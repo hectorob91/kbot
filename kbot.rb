9a5e853b8c46d85f8dcce8b35ef52c44dac520ca
Hector Ortega <hectorob91@gmail.com>
2020-11-17 23:34:22 +0000

    Ahora el usuario puede ingresar cantidad, precio unitario y el estado

--- kbot.rb
+++ kbot.rb
@@ -1,14 +1,17 @@
 class FacturaBot
-    def initialize(cantidad, precioUnitario)
+    def initialize(cantidad, precioUnitario, estado)
         @cantidad = cantidad
         @precioUnitario = precioUnitario
+        @estado = estado
     end
 
     def calcular
-        "Esta es cantidad: #{@cantidad} y este es precio unitario: #{@precioUnitario}"
+        "Esta es cantidad: #{@cantidad}\n
+        Este es precio unitario: #{@precioUnitario}\n
+        Este es el estado: #{@estado}"
     end
 end
 
-calculoBot = FacturaBot.new(ARGV[0], ARGV[1])
+calculoBot = FacturaBot.new(ARGV[0], ARGV[1], ARGV[2])
 
 puts calculoBot.calcular()
