96512d7bb8384cdb3e8005634068341da0bcb17c
Hector Ortega <hectorob91@gmail.com>
2020-11-17 23:27:25 +0000

    Recibe e imprime cantidad del usuario

--- kbot.rb
+++ kbot.rb
@@ -1,1 +1,13 @@
-puts "Hola, mundo"
+class FacturaBot
+    def initialize(cantidad)
+        @cantidad = cantidad
+    end
+
+    def calcular
+        @cantidad
+    end
+end
+
+calculoBot = FacturaBot.new(ARGV[0])
+
+puts calculoBot.calcular()
